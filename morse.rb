9ca2c44ccf32755183a13cf09150e630da4d576d
francksefu <90141102+francksefu@users.noreply.github.com>
2023-08-24 00:08:43 +0200

    Space every word

--- morse.rb
+++ morse.rb
@@ -1,26 +1,27 @@
 require_relative 'morse_to_alphabet'
 
-def decode_character character_in_morse_code
+def decode_character(character_in_morse_code)
   MORSE_CODE[character_in_morse_code]
 end
 
-def decode_word word_in_morse_code
-  word_array = word_in_morse_code.split(' ')
+def decode_word(word_in_morse_code)
+  word_array = word_in_morse_code.split
   message = []
-  for character in word_array
+  word_array.each do |character|
     message.push decode_character character
   end
-  new_message = message.join(' ')
+  new_message = message.join
 end
 
-def decode_entire_message decode_entire_message
-  message_array = decode_entire_message.split(' ')
+def decode_entire_message(decode_entire_message)
+  message_array = decode_entire_message.split('  ')
   message = []
-  for word in message_array
+  message_array.each do |word|
     message.push decode_word word
   end
   new_message = message.join(' ')
   print new_message
 end
 
-decode_entire_message "-- -.--   -. .- -- ."
+decode_entire_message('-- -.--   -. .- -- .')
+decode_entire_message('      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
