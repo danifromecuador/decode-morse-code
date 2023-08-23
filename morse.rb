0edc64b69a9c7299d2f254fa907a5c789cff22dd
dani <danifromecuador@gmail.com>
2023-08-23 13:08:58 -0500

    Create a method that decode a morse word

--- morse.rb
+++ morse.rb
@@ -1,7 +1,14 @@
 require_relative 'morse_to_alphabet'
 
-def decode_character message_in_morse_code
-  MORSE_CODE[message_in_morse_code]
+def decode_character character_in_morse_code
+  MORSE_CODE[character_in_morse_code]
 end
 
-puts decode_character '.-'
+def decode_word word_in_morse_code
+  word_array = word_in_morse_code.split(' ')
+  message = []
+  for character in word_array
+    message.push decode_character character
+  end
+  new_message = message.join(' ')
+end
